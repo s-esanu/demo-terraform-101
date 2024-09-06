ff5e63669edb1091d784a0b1843eecf9e9e62e4a
Nicu Esanu <Sebastian.Esanu@ibm.com>
2024-09-06 18:53:50 +0300

    make a few notes on the setup script

--- assets/setup-web.sh
+++ assets/setup-web.sh
@@ -3,6 +3,7 @@
 cp /tmp/assets/webapp /usr/local/bin/
 chmod +x /usr/local/bin/*
 cp /tmp/assets/webapp.service /lib/systemd/system/webapp.service
-service webapp start
-systemctl enable webapp
-
+# not sure about the service file location
+# service webapp start - start service
+systemctl enable --now webapp 
+# enable + start service
